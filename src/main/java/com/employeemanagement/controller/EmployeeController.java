5c9200f93225c1f89ec581ab14aac51df1271159
Rajbhoj2309 <kiranrajbhoj89@gmail.com>
2023-12-20 15:11:41 +0530

    ADD post, delete , update and patch in Employees operation

--- src/main/java/com/employeemanagement/controller/EmployeeController.java
+++ src/main/java/com/employeemanagement/controller/EmployeeController.java
@@ -5,9 +5,15 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -54,6 +60,54 @@ public class EmployeeController {
 //        return ResponseEntity.ok(departments);
 			return ResponseEntity.of(Optional.of(employees));
 		}
+	}
+	
+	@PostMapping("/add")
+    public ResponseEntity<Employees> createDepartment(@RequestBody Employees employees) {
+        if (employees != null) {
+        	Employees createdEmployees = employeeRepository.save(employees);
+            return ResponseEntity.status(HttpStatus.CREATED).body(createdEmployees);
+        } else {
+            return ResponseEntity.badRequest().build();
+        }
+    }
+	
+	@PutMapping("/update/{empno}")
+	public ResponseEntity<Employees> updateEmployee(@PathVariable("empno") int empno ,
+			@RequestBody Employees employeesUpdate){
+		if(!employeeRepository.existsById(empno)) {
+			return ResponseEntity.notFound().build();
+		}
+		employeesUpdate.setEmpNo(empno);
+			Employees updateEmployee = employeeRepository.save(employeesUpdate);
+		return ResponseEntity.status(HttpStatus.OK).body(updateEmployee);
+	}
+	
+	@DeleteMapping("/delete/{empno}")
+	public ResponseEntity<Employees> deleteEmployee(@PathVariable("empno") int empno){
+		if(employeeRepository.existsById(empno)) {
+			employeeRepository.deleteById(empno);
+			return ResponseEntity.status(HttpStatus.OK).build();
+		}
+
+		return ResponseEntity.noContent().build();
+	}
+	
+	@PatchMapping("/patch/{empno}")
+	public ResponseEntity<Employees> patchEmployee(@PathVariable("empno")int empno,
+				@RequestBody Employees employees){
+		
+		if(employeeRepository.existsById(empno)) {
+			employees.setEmpNo(empno);
+			Employees save = employeeRepository.save(employees);
+			return ResponseEntity.status(HttpStatus.OK).body(save);
+		}else if(!employeeRepository.existsById(empno)) {
+			employees.setEmpNo(empno);
+			Employees save = employeeRepository.save(employees);
+			return ResponseEntity.status(HttpStatus.OK).body(save);
+		}
+		
+		return ResponseEntity.notFound().build();
 	}
 
 }
